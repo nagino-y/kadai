721e751431c4e8f2fa5b7c3c1abb0a1b4c3fa5fe
Yukichi <naginoyukichi@gmail.com>
2022-03-08 00:07:04 +0900

    gulp gazou assyukku

--- gulpfle.js
+++ gulpfle.js
@@ -1,17 +1,17 @@
-const gulp = require("gulp");
-const sass = require("gulp-sass")(require("sass"));
+// const gulp = require("gulp");
+// const sass = require("gulp-sass")(require("sass"));
 
-gulp.task("default", function() {
-  return gulp.watch("css/style.scss", function(){
-    return(
-      gulp
-        .src("css/style.scss")
-        .pipe(
-          sass({
-            outputStyle: "expanded"
-          })
-        )
-        .pipe(gulp.dest("css"))
-    );
-  });
-});
+// gulp.task("default", function() {
+//   return gulp.watch("css/style.scss", function(){
+//     return(
+//       gulp
+//         .src("css/style.scss")
+//         .pipe(
+//           sass({
+//             outputStyle: "expanded"
+//           })
+//         )
+//         .pipe(gulp.dest("css/out"))
+//     );
+//   });
+// });
